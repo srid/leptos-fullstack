acbca9390e74d8b6b9d5271861415736a73c2646
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2023-07-25 15:17:32 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+
+    # Dev tools
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem = { config, self', pkgs, lib, system, ... }:
+        let
+          cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+          nonRustDeps = [
+            pkgs.libiconv
+          ];
+        in
+        {
+          # Rust package
+          packages.default = pkgs.rustPlatform.buildRustPackage {
+            inherit (cargoToml.package) name version;
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+
+          # Rust dev environment
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [
+              config.treefmt.build.devShell
+            ];
+            shellHook = ''
+              # For rust-analyzer 'hover' tooltips to work.
+              export RUST_SRC_PATH=${pkgs.rustPlatform.rustLibSrc}
+            '';
+            buildInputs = nonRustDeps;
+            nativeBuildInputs = with pkgs; [
+              just
+              rustc
+              cargo
+              cargo-watch
+              rust-analyzer
+            ];
+          };
+
+          # Add your auto-formatters here.
+          # cf. https://numtide.github.io/treefmt/
+          treefmt.config = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixpkgs-fmt.enable = true;
+              rustfmt.enable = true;
+            };
+          };
+        };
+    };
+}
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author = "Sridhar Ratnakumar", version, about)]
+/// Application configuration
+struct Args {
+    /// whether to be verbose
+    #[arg(short = 'v')]
+    verbose: bool,
+
+    /// an optional name to green
+    #[arg()]
+    name: Option<String>,
+}
+
+fn main() {
+    let args = Args::parse();
+    if args.verbose {
+        println!("DEBUG {args:?}");
+    }
+    println!("Hello {}!", args.name.unwrap_or("world".to_string()));
+}
