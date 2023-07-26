6dfed22ff27405b43ad79302f57f7f489fc79935
Sridhar Ratnakumar <srid@srid.ca>
2023-07-25 21:56:32 -0400

    frontend: link to backend /api

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -11,7 +11,16 @@ fn start_app() {
         .and_then(|win| win.document())
         .expect("Could not access document");
     let body = document.body().expect("Could not access document.body");
-    let text_node = document.create_text_node("Hello, world from leptos-fullstack (vanilla Rust)!");
+    let text_node = document.create_text_node("Hello! To visit backend page, click: ");
+    let alink = document
+        .create_element("a")
+        .expect("Failed to create <a> element");
+    alink
+        .set_attribute("href", "/api")
+        .expect("Failed to set href");
+    alink.set_inner_html("/api");
     body.append_child(text_node.as_ref())
         .expect("Failed to append text");
+    body.append_child(alink.as_ref())
+        .expect("Failed to append link");
 }
