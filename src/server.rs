2a13c8470353e0da056cd2eb0b5b08542709f412
Sridhar Ratnakumar <srid@srid.ca>
2023-08-01 18:51:39 -0400

    Explicitly handle errors, so in future we can serve the app error view (#13)

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,12 @@
 use crate::app::App;
 use crate::thing::{ReadThings, Thing};
+use axum::http::StatusCode;
+use axum::{
+    body::Body,
+    extract::State,
+    http::{Request, Uri},
+    response::IntoResponse,
+};
 use axum::{
     routing::{get, post},
     Router,
@@ -16,10 +23,17 @@ pub async fn main() {
     let routes = generate_route_list(|cx| view! { cx, <App/> }).await;
     let client_dist = ServeDir::new(leptos_options.site_root.clone());
     let app = Router::new()
+        // custom routes
         .route("/hello", get(root))
+        // server functions API routes
         .route("/api/*fn_name", post(leptos_axum::handle_server_fns))
+        // application routes
         .leptos_routes(&leptos_options, routes, |cx| view! { cx, <App/> })
-        .fallback_service(client_dist)
+        // when none of the routes match the requested URL
+        .fallback(error_handler)
+        // static files are served as fallback (but *before* falling back to
+        // error handler)
+        .fallback_service(client_dist.clone())
         .with_state(leptos_options);
     println!("Launching http://{}", &addr);
     println!("fn_url: {}", ReadThings::url());
@@ -27,6 +41,18 @@ pub async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+use axum::response::Response as AxumResponse;
+
+pub async fn error_handler(
+    _uri: Uri,
+    State(options): State<LeptosOptions>,
+    _req: Request<Body>,
+) -> AxumResponse {
+    // TODO: Let app render error page
+    let _handler =
+        leptos_axum::render_app_to_stream(options.to_owned(), move |cx| view! {cx, <App/>});
+    (StatusCode::NOT_FOUND, "'tis not found").into_response()
 }
 
 #[debug_handler]
