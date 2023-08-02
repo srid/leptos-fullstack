06c59dad9238318a9b05e446f33d1d9f70275c09
Sridhar Ratnakumar <srid@srid.ca>
2023-08-02 12:06:05 -0400

    Remove redundant code

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,3 @@ mod thing;
 async fn main() {
     server::main().await
 }
-
-#[cfg(not(feature = "ssr"))]
-pub fn main() {
-    // no client-side main function
-    // unless we want this to work with e.g., Trunk for a purely client-side app
-    // see lib.rs for hydration function instead
-}
